8defdff702842e0bb39b7134bd4a414171def18f
Jona Reyes <jonamreyes@gmail.com>
2021-12-04 20:09:55 -0500

    added cart-item component and updated cart-dropdown

--- src/App.js
+++ src/App.js
@@ -62,6 +62,7 @@ class App extends React.Component {
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           {/* <Route path='/signin' component={SignInAndSignUpPage} /> */}
+          
           <Route exact path='/signin' render={() => this.props.currentUser ? (
             <Redirect to='/' />
           ) : (
