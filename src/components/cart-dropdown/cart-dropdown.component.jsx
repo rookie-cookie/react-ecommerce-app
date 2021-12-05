4e169ac03c1ee436d4b83e57ef01673a1dc7f634
Jona Reyes <jonamreyes@gmail.com>
2021-12-04 21:32:01 -0500

    added reselect package to memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import CustomButton from '../custom-button/custom-button.component'
-import './cart-dropdown.styles.scss'
-import CartItem from '../cart-item/cart-item.component'
-import { connect } from 'react-redux'
+import React from 'react';
+import { connect } from 'react-redux';
+
+import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+
+import './cart-dropdown.styles.scss';
 
 const CartDropdown = ({ cartItems }) => (
   <div className='cart-dropdown'>
@@ -15,8 +18,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(CartDropdown);
--- src/index.js
+++ src/index.js
@@ -9,11 +9,12 @@ import App from './App';
 ReactDOM.render(
   //Provider - parent of everything inside the application
   //allows to get access to all the thing related to the store
-  <Provider store={store} > 
-    <BrowserRouter>
-     <App />
-    </BrowserRouter>
-  </Provider>
+    <Provider store={store} > 
+      <BrowserRouter>
+       <App />
+      </BrowserRouter>
+    </Provider>
+
   ,
   document.getElementById('root')
 );
