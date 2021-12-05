4070d616109d729427c9b5b6be9a8a5b76e54a35
Jona Reyes <jonamreyes@gmail.com>
2021-12-04 22:05:17 -0500

    setup checkout page and withrouter to checkout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom'
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import CheckoutPage from './pages/checkout/checkout.component'
+
 import { auth, createUserProfileDocument} from './firebase/firebase.utils'
 import { connect } from 'react-redux'
 import { setCurrentUser } from './redux/user/user.actions'
 import { createStructuredSelector } from 'reselect'
 import { selectCurrentUser } from './redux/user/user.selectors'
+
 
 class App extends React.Component {
   // constructor() {
@@ -63,6 +67,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           {/* <Route path='/signin' component={SignInAndSignUpPage} /> */}
           
           <Route exact path='/signin' render={() => this.props.currentUser ? (
