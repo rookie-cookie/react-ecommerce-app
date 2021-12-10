96133bdc617d5c32dcd56e4410c9ebb56022f704
Jona Reyes <jonamreyes@gmail.com>
2021-12-09 20:53:13 -0500

    added checkout page, checkout item component and updated cart to use selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,10 +6,11 @@ import CartItem from '../cart-item/cart-item.component';
 import { createStructuredSelector } from 'reselect'
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import { withRouter } from 'react-router-dom'
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {
@@ -23,7 +24,10 @@ const CartDropdown = ({ cartItems, history }) => (
         )
     }
     </div>
-    <CustomButton onClick={()=> history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+    <CustomButton onClick={()=> {
+      history.push('/checkout');
+      dispatch(toggleCartHidden())
+      }}>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
