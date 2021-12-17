5ba5fedb4dd7ca23dfe6dae377389eb079e86b26
Jona Reyes <jonamreyes@gmail.com>
2021-12-17 17:19:53 -0500

    setup redux for the directory and collection data

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,7 +5,9 @@ import { persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'  //returns the local storage of window object which will be used as the main storage for our cart items for now
 
 import userReducer from './user/user-reducer'
-import cartReducer from './cart/cart.reducer';
+import cartReducer from './cart/cart.reducer'
+import directoryReducer from './directory/directory.reducer'
+import shopReducer from './shop/shop.reducer'
 
 const persistConfig = {
   key: 'root',
@@ -15,7 +17,9 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer
 })
 
 export default persistReducer(persistConfig, rootReducer)
